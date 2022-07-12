dbac8d601429ada33ddab66c35c90a434a1ea6ae
doinki <doinki94@gmail.com>
2022-07-12 11:09:53 +0900

    🎉

---
+++ README.md
@@ -0,0 +1,21 @@
+# eslint-config-sandbox
+
+## Installation
+
+```bash
+# yarn
+yarn add -D eslint-config-sandbox
+
+# npm
+npm i -D eslint-config-sandbox
+```
+
+## Configuration
+
+- [airbnb-typescript](https://github.com/iamturns/eslint-config-airbnb-typescript#4-configure-the-eslint-typescript-parser)
+
+```json
+{
+  "parserOptions": { "project": "tsconfig.json" }
+}
+```
---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: { browser: true },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@next/next/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:tailwindcss/recommended',
+  ],
+  plugins: [
+    'simple-import-sort',
+    'sort-destructure-keys',
+    'sort-keys-fix',
+    'trim',
+    'typescript-sort-keys',
+  ],
+  rules: {
+    '@typescript-eslint/consistent-type-exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    'jsx-a11y/alt-text': ['error', { img: ['Image'] }],
+    'jsx-a11y/img-redundant-alt': ['error', { components: ['Image'] }],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: ['arrow-function'],
+        unnamedComponents: ['arrow-function'],
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-sort-props': [
+      'warn',
+      { callbacksLast: true, reservedFirst: true, shorthandLast: true },
+    ],
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    'simple-import-sort/exports': 'warn',
+    'simple-import-sort/imports': 'warn',
+    'sort-destructure-keys/sort-destructure-keys': 'warn',
+    'sort-keys-fix/sort-keys-fix': 'warn',
+    'tailwindcss/classnames-order': ['warn', { officialSorting: true }],
+    'trim/class-name': 'warn',
+    'typescript-sort-keys/interface': 'warn',
+  },
+  settings: {
+    'import/resolver': { typescript: { alwaysTryTypes: true } },
+  },
+};
